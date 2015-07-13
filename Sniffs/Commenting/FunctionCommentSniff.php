9e059917eebbbb6f3d3a7d49c98bf95c59057de2
Mte90 <mte90smanettator@gmail.com>
2015-07-13 21:37:58 +0200

    return statement support for void

--- Sniffs/Commenting/FunctionCommentSniff.php
+++ Sniffs/Commenting/FunctionCommentSniff.php
@@ -72,9 +72,11 @@ class PHPDoc_Sniffs_Commenting_FunctionCommentSniff extends PEAR_Sniffs_Commenti
 
         if ($return !== null) {
             $content = $tokens[($return + 2)]['content'];
-            if (empty($content) === true || $tokens[($return + 2)]['code'] !== T_DOC_COMMENT_STRING) {
-                $error = 'Return type missing for @return tag in function comment';
-                $phpcsFile->addError($error, $return, 'MissingReturnType');
+            if (empty($content) === true ) {
+                if(trim($tokens[($return + 2)]['content']) !== 'void' || $tokens[($return + 2)]['code'] !== T_DOC_COMMENT_STRING) {
+		    $error = 'Return type missing for @return tag in function comment';
+		    $phpcsFile->addError($error, $return, 'MissingReturnType');
+                }
             } else {
                 // Check return type (can be multiple, separated by '|').
                 $typeNames      = explode('|', $content);
